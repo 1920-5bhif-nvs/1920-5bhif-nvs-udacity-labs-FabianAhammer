c13374fac6bfffb372169ee47f9cbadc9f6b60d4
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2019-12-25 13:06:20 +0100

    Added binding Exercise 2-6 FabianAhammer

--- Lessons/AboutMe/app/src/main/java/at/htl/aboutme/MainActivity.kt
+++ Lessons/AboutMe/app/src/main/java/at/htl/aboutme/MainActivity.kt
@@ -8,33 +8,38 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import at.htl.aboutme.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     public lateinit var  binding: ActivityMainBinding
+    private val myName: MyName = MyName("Fabian Ahammer")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.done_btn).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneBtn.setOnClickListener {
             addNickname(it)
         }
     }
     private fun addNickname(view: View){
-        var editText = findViewById<EditText>(R.id.nick_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nick_text)
+        binding.apply {
+            myName?.nickname = nickEdit.text.toString()
+            invalidateAll()
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-        view.visibility = View.GONE
+            nickEdit.visibility = View.GONE
+            doneBtn.visibility = View.GONE
+            nickText.visibility = View.VISIBLE
 
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+            // Hide the keyboard.
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
 
 }
+data class MyName(var name: String = "", var nickname: String = "")
