a22a20320a606946ef79951ea28c84690cbcd2d9
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2020-01-04 14:46:09 +0100

    Added Safe Args Exercise 3-8 FabianAhammer

--- Lessons/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Lessons/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -18,7 +18,7 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater,R.layout.fragment_title,container,false
         )
-        binding.playButton.setOnClickListener (Navigation.createNavigateOnClickListener((R.id.action_titleFragment_to_gameFragment)))
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(TitleFragmentDirections.actionTitleFragmentToGameFragment()))
         setHasOptionsMenu(true)
 
         return binding.root
