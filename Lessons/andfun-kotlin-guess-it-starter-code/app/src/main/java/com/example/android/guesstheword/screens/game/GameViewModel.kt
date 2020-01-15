0053dfa7c415d657c197a5f362a81db1d71ebcc1
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2020-01-15 12:29:11 +0100

    Added encapsulation Exercise 5-5 FabianAhammer
    Added lifedata Exercise 5-4 FabianAhammer

--- Lessons/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Lessons/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,24 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
 
     // The current word
-    val word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
 
-    // The current score
-    val score = MutableLiveData<Int>()
+    val word : LiveData<String>
+        get() = _word
+    // The current score// internal
+    private val _score = MutableLiveData<Int>()
+    //external
+    val score: LiveData<Int>
+        get() {
+            return _score
+        }
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -18,8 +26,8 @@ class GameViewModel : ViewModel(){
         Log.i("GameViewModel","GameViewModel created!")
         resetList()
         nextWord()
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -29,12 +37,12 @@ class GameViewModel : ViewModel(){
 
 
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
@@ -70,7 +78,7 @@ class GameViewModel : ViewModel(){
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 }
