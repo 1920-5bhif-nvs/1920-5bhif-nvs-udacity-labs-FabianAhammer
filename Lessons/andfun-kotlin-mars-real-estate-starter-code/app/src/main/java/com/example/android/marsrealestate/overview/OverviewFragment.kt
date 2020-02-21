3a7957d1424d04eba2db8c4e9cbca7af55c7e899
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2020-02-21 14:44:19 +0100

    Added adapter Exercise 8-5 FabianAhammer

--- Lessons/andfun-kotlin-mars-real-estate-starter-code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ Lessons/andfun-kotlin-mars-real-estate-starter-code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -1,20 +1,3 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.example.android.marsrealestate.overview
 
 import android.os.Bundle
@@ -23,7 +6,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
-import com.example.android.marsrealestate.databinding.GridViewItemBinding
 
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
@@ -43,13 +25,17 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = GridViewItemBinding.inflate(inflater)
+        val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.setLifecycleOwner(this)
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
+        // Sets the adapter of the photosGrid RecyclerView
 
         setHasOptionsMenu(true)
         return binding.root
