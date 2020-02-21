eb159df649b8689c69c396ec3103e3bd99e42ac2
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2020-02-21 10:00:45 +0100

    Added adapter to fragment Exercise 7-2 FabianAhammer

--- Lessons/andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Lessons/andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,17 +16,21 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
 
     var data = listOf<SleepNight>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        set(value){
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun getItemCount(): Int {
        return data.size
@@ -36,5 +40,12 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
         val item = data[position]
         holder.textView.text = item.sleepQuality.toString()
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view,parent,false) as TextView
+        return TextItemViewHolder(view)
+    }
+
 
 }
