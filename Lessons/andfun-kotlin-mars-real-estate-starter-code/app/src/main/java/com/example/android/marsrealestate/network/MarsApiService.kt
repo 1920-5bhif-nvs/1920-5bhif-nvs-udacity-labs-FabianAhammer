0177c9d5e3fed0ee1f8cbfe6eaede541027fa161
FabianAhammer <f.ahammer@students.htl-leonding.ac.at>
2020-02-21 14:22:34 +0100

    Added glide and image view Exercise 8-4 FabianAhammer

--- Lessons/andfun-kotlin-mars-real-estate-starter-code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Lessons/andfun-kotlin-mars-real-estate-starter-code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -1,57 +1,53 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 
 package com.example.android.marsrealestate.network
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import kotlinx.coroutines.Deferred
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import kotlinx.coroutines.Deferred
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
-
+/**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
 private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
 
-
-// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+/**
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
+ */
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .baseUrl(BASE_URL)
         .build()
 
-
-// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
+/**
+ * A public interface that exposes the [getProperties] method
+ */
 interface MarsApiService {
+    /**
+     * Returns a Coroutine [Deferred] [List] of [MarsProperty] which can be fetched with await() if
+     * in a Coroutine scope.
+     * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
+     * HTTP method
+     */
     @GET("realestate")
     fun getProperties():
+    // The Coroutine Call Adapter allows us to return a Deferred, a Job with a result
             Deferred<List<MarsProperty>>
 }
 
-// TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
 object MarsApi {
-    val retrofitService : MarsApiService by lazy {
-        retrofit.create(MarsApiService::class.java)
-    }
+    val retrofitService : MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
 }
